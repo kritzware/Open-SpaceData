d1d80af773d7fb9b4f2d3ad4dd3256e911a037d7
Louis Bluhm <suspiciouspotato@gmail.com>
2016-01-04 17:35:14 +0000

    Fixed JSON link, updated form control

--- key.js
+++ key.js
@@ -1,36 +1,3 @@
-var API_KEY = "Fof8jxqZUUxa7MKmuWjgiiQTyiumR4tbURMclOxK";
-
-var test_json = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-01-01&camera=fhaz&api_key=Fof8jxqZUUxa7MKmuWjgiiQTyiumR4tbURMclOxK";
-
-var link = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-01-01&camera=fhaz&api_key=Fof8jxqZUUxa7MKmuWjgiiQTyiumR4tbURMclOxK";
-var link2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-01-01&camera=fhaz&api_key=Fof8jxqZUUxa7MKmuWjgiiQTyiumR4tbURMclOxK?jsoncallback=?"
-
-
-function getData() {
-	var link = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/"
-}
-
-
-$.getJSON( "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-01-01&camera=fhaz&api_key=Fof8jxqZUUxa7MKmuWjgiiQTyiumR4tbURMclOxK&callback=?json", function( json ) {
-  
-
-
-  // var earthdate = json.photos[0].earth_date;
-  // var id = json.photos[0].id;
-  // var img = json.photos[0].img_src;
-  // var cameratype = json.photos[0].camera.full_name;
-  // var sol = json.photos[0].sol;
-
-  // document.getElementById('earth-date').innerHTML = "<b>Date:</b> " + earthdate;
-  // document.getElementById('rover-img').src = img;
-  // document.getElementById('camera-type').innerHTML = "<b>Camera Type: </b>" + cameratype;
-  // document.getElementById('sol').innerHTML = "<b>SOL: </b>" + sol;
-  // document.getElementById('image-id').innerHTML = "<b>Image ID: </b>" + id;
- });
-
-
-
-
 $(document).ready(function() {
   $('.input-group.date').datepicker({
       format: "yyyy-mm-dd",
@@ -38,3 +5,45 @@ $(document).ready(function() {
       orientation: "bottom auto"
   });
 });
+
+function getData() {
+	var link_p1 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=";
+	var link_p2 = "&camera=";
+	var api_key = "&api_key=Fof8jxqZUUxa7MKmuWjgiiQTyiumR4tbURMclOxK";
+	var request = "&callback=?json";
+
+	var date = ($('#earth-date').val());
+	console.log("DATE: ", date);
+	var camera = ($('#cameras input:radio:checked').val());
+	console.log("CAMERA: ", camera);
+
+	var constructed_link = link_p1 + date + link_p2 + camera + api_key + request;
+	console.log(constructed_link);
+
+	$.getJSON(constructed_link, function(json) {
+
+		  var earthdate = json.photos[0].earth_date;
+		  var id = json.photos[0].id;
+		  var img = json.photos[0].img_src;
+		  var cameratype = json.photos[0].camera.full_name;
+		  var sol = json.photos[0].sol;
+
+		  document.getElementById('earth-date').innerHTML = "<b>Date:</b> " + earthdate;
+		  document.getElementById('rover-img').src = img;
+		  document.getElementById('camera-type').innerHTML = "<b>Camera Type: </b>" + cameratype;
+		  document.getElementById('sol').innerHTML = "<b>SOL: </b>" + sol;
+		  document.getElementById('image-id').innerHTML = "<b>Image ID: </b>" + id;
+
+		$('#results').css({ 
+			'visibility': 'visible'
+		});
+	});
+
+}
+
+
+
+
+
+
+
